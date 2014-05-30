18d03196ed748066ce65d3bd9fecc4f567233967
Alec Rippberger <arippberger@gmail.com>
2014-05-30 12:58:50 -0500

    removed default loop from index.php

--- index.php
+++ index.php
@@ -19,31 +19,6 @@ get_header(); ?>
 
 				<div ng-view></div>
 
-					<?php if ( have_posts() ) : ?>
-
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
-
-							<?php
-								/* Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'content', get_post_format() );
-							?>
-
-						<?php endwhile; ?>
-
-						<?php sharp__activate_paging_nav(); ?>
-
-						<?php else : ?>
-
-							<?php get_template_part( 'content', 'none' ); ?>
-
-						<?php endif; ?>
-
-					</div><!--.col-sm-8 .blog-main -->
-
 			</main><!-- #main -->
 		</div><!-- #primary -->
 
